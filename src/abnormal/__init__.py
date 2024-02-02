ad592d63a77ba5214a27723790ff355f923f2274
David Barts <david.w.barts@gmail.com>
2024-02-01 18:55:38 -0800

    Correct defects found in testing with sqlite3.

--- src/abnormal/__init__.py
+++ src/abnormal/__init__.py
@@ -8,8 +8,9 @@ from .todb import convert
 # C l a s s e s
 
 class Connection:
-    def __init__(self, raw):
+    def __init__(self, raw, paramstyle):
         self.raw = raw
+        self.paramstyle = paramstyle
 
     def close(self):
         self.raw.close()
@@ -23,14 +24,13 @@ class Connection:
     def cursor(self):
         return Cursor(self.raw.cursor(), self)
 
-    def execute(self, query, params):
+    def execute(self, query, params={}):
         return self.cursor().execute(query, params)
 
 class Cursor:
     def __init__(self, raw, parent):
         self.raw = raw
         self.parent = parent
-        self._rowcount = 0
         self._colnames = None
 
     def callproc(self, procname, params):
@@ -39,16 +39,14 @@ class Cursor:
     def close(self):
         self.raw.close()
 
-    def execute(self, operation, params):
-        self.raw.execute(*convert(operation, params, parent.paramstyle))
-        self._rowcount = self.raw.rowcount
+    def execute(self, operation, params={}):
+        self.raw.execute(*convert(operation, params, self.parent.paramstyle))
         self._colnames = [ x[0] for x in self.raw.description ]
         return self
 
     # executemany not supported (for now)
 
     def into(self, target):
-        self._rowcount = 0  # make .into1 fail
         while True:
             value = self._into(target)
             if value is None:
@@ -56,10 +54,13 @@ class Cursor:
             yield value
 
     def into1(self, target):
-        if self._rowcount != 1:
-            raise Error(f"unexpected row count of {self._rowcount}")
-        self._rowcount = 0
-        return self._into(target)
+        ret = self._into(target)
+        # XXX - some interfaces always return -1 for rowcount
+        if abs(self.raw.rowcount) != 1:
+            raise Error(f"unexpected row count of {self.raw.rowcount}")
+        if ret is None:
+            raise Error("unexpected lack of results")
+        return ret
 
     def _into(self, target):
         row = self.raw.fetchone()
@@ -73,6 +74,11 @@ class Cursor:
         return target(**kwargs)
 
 # F u n c t i o n s
+
+def connect(mod, *args, **kwargs):
+    """Given a PEP 249 compliant database module and connection parameters,
+       return am abnormal Connection object."""
+    return Connection(mod.connect(*args, **kwargs), mod.paramstyle)
 
 def mapping(**kwargs):
     "For returning a mapping with .into()"
